1deeca46f1b60448e569ff4298d691d727a882d4
hugosenari <hugosenari@gmail.com>
2022-03-12 02:37:28 -0300

    chore: re index dbus

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''dbus bindings for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-dbus-master.flake = false;
+  inputs.src-dbus-master.owner = "zielmicha";
+  inputs.src-dbus-master.ref   = "refs/heads/master";
+  inputs.src-dbus-master.repo  = "nim-dbus";
+  inputs.src-dbus-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-dbus-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-dbus-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
